a872a34408617ac35ade9e1f4ec382d446bf610c
Ivan Moore <ivan@teamoptimization.com>
2024-04-14 12:21:41 +0100

    trivial html change

--- src/main/java/com/oocode/HomePage.java
+++ src/main/java/com/oocode/HomePage.java
@@ -4,7 +4,7 @@ public class HomePage {
     public static final String HTML = """
             <html>
                 <body>
-                <h1>Answers questions</h1>
+                <h1>This Answers questions</h1>
                 <p>This is a minimal web app for a lab</p>
                 </body>
             </html>""".trim();
